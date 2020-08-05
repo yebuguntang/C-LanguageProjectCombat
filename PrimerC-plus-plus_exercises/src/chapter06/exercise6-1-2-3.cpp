059fc3c1b5a8b3764eb9b3670b45e81606793f75
云主宰苍穹 <2694048168@qq.com>
2020-08-06 00:12:55 +0800

    create the chapter06

---
+++ PrimerC-plus-plus_exercises/src/chapter06/exercise6-1-2-3.cpp
@@ -0,0 +1,36 @@
+/* exercise 6-1、6-2、6-3
+** 练习6.1: 实参和形参的区别是什么?
+** solution: 
+**
+** 练习6.2: 请指出下列函数哪个有错误，为什么?应该如何修改这些错误呢?
+** (a) int f() {
+**         string s;
+**         //...
+**         return s;
+**     }
+** (b) f2(int i) {  }
+** (c) int calc(int v1，int v1) }
+** (d) double square (double x) return x * x;
+**
+** 练习6.3: 编写你自己的fact函数，上机检查 是否正确。
+**
+*/
+
+#include <iostream>
+
+// solution 6-3
+
+int main()
+{
+    // solution
+
+    return 0;
+}
+
+/* 编译命令操作流程
+** 0、打开终端 terminal，VSCode使用 Ctrl+shift+`; Ubuntu使用 Ctrl+Alt+T
+** 1、进入当前源文件路径，cd src; cd chapter01
+** 2、编译源代码文件，g++ --version; g++ -o exercise exercise6-1-2-3.cpp
+** 3、运行生成的可执行程序，exercise; Ubuntu使用 ./exercise
+** 4、删除生成的可执行程序，rm -rf exercise.exe; Ubuntu使用 rm -rf exercise
+*/
---
+++ PrimerC-plus-plus_exercises/src/chapter06/exercise6-4-5.cpp
@@ -0,0 +1,29 @@
+/* exercise 6-4、6-5
+** 练习6.4: 编写一个与用户交互的函数，要求用户输入一个数字，计算生成该数字的阶乘
+** 在main函数中调用该函数
+**
+** 练习6.5: 编写一个函数输出其实参的绝对值。
+**
+*/
+
+#include <iostream>
+
+// solution 6-4
+
+
+// solution 6-5
+
+int main()
+{
+    // solution
+
+    return 0;
+}
+
+/* 编译命令操作流程
+** 0、打开终端 terminal，VSCode使用 Ctrl+shift+`; Ubuntu使用 Ctrl+Alt+T
+** 1、进入当前源文件路径，cd src; cd chapter01
+** 2、编译源代码文件，g++ --version; g++ -o exercise exercise6-4-5.cpp
+** 3、运行生成的可执行程序，exercise; Ubuntu使用 ./exercise
+** 4、删除生成的可执行程序，rm -rf exercise.exe; Ubuntu使用 rm -rf exercise
+*/
